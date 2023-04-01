04cb5745267448580a1947f01237f31ad414388a
Shrikar Tamirisa <117923706+shrikar-tamirisa@users.noreply.github.com>
2023-04-01 15:53:05 +0530

    Add files via upload

---
+++ org_cam_hands.py
@@ -0,0 +1,127 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+import tensorflow as tf
+import cv2
+import mediapipe as mp
+from keras.models import load_model
+import numpy as np
+import time
+import pandas as pd
+import streamlit as st
+
+
+model = load_model(r"C:\Users\Shrikar\Desktop\Sign-Language-Detection\H5 files\small_batch_val-10.h5")
+st.title("The dumb guy is saying:")
+t = st.empty()
+msg = ""
+
+mphands = mp.solutions.hands
+hands = mphands.Hands()
+mp_drawing = mp.solutions.drawing_utils
+cap = cv2.VideoCapture(0)
+
+_, frame = cap.read()
+
+h, w, c = frame.shape
+
+img_counter = 0
+analysisframe = ''
+# letterpred = list(let)
+letterpred = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
+while True:
+    _, frame = cap.read()
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 13:
+        # Enter pressed
+        print("\n")
+        continue
+    elif k%256 == 32:
+        # SPACE pressed
+        analysisframe = frame
+        showframe = analysisframe
+        cv2.imshow("Frame", showframe)
+        framergbanalysis = cv2.cvtColor(analysisframe, cv2.COLOR_BGR2RGB)
+        resultanalysis = hands.process(framergbanalysis)
+        hand_landmarksanalysis = resultanalysis.multi_hand_landmarks
+        if hand_landmarksanalysis:
+            for handLMsanalysis in hand_landmarksanalysis:
+                x_max = 0
+                y_max = 0
+                x_min = w
+                y_min = h
+                for lmanalysis in handLMsanalysis.landmark:
+                    x, y = int(lmanalysis.x * w), int(lmanalysis.y * h)
+                    if x > x_max:
+                        x_max = x
+                    if x < x_min:
+                        x_min = x
+                    if y > y_max:
+                        y_max = y
+                    if y < y_min:
+                        y_min = y
+                y_min -= 20
+                y_max += 20
+                x_min -= 20
+                x_max += 20 
+
+        analysisframe = cv2.cvtColor(analysisframe, cv2.COLOR_BGR2GRAY)
+        analysisframe = analysisframe[y_min:y_max, x_min:x_max]
+        analysisframe = cv2.resize(analysisframe,(28,28))
+
+
+        nlist = []
+        rows,cols = analysisframe.shape
+        for i in range(rows):
+            for j in range(cols):
+                k = analysisframe[i,j]
+                nlist.append(k)
+        
+        datan = pd.DataFrame(nlist).T
+        colname = []
+        for val in range(784):
+            colname.append(val)
+        datan.columns = colname
+
+        pixeldata = datan.values
+        pixeldata = pixeldata / 255
+        pixeldata = pixeldata.reshape(-1,28,28,1)
+        prediction = model.predict(pixeldata)
+        predarray = np.array(prediction[0])
+        t.markdown(letterpred[np.where(predarray == (max(predarray)))[0][0]])
+        # print(predarray)
+        time.sleep(1)
+    
+    framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = hands.process(framergb)
+    hand_landmarks = result.multi_hand_landmarks
+    if hand_landmarks:
+        for handLMs in hand_landmarks:
+            x_max = 0
+            y_max = 0
+            x_min = w
+            y_min = h
+            for lm in handLMs.landmark:
+                x, y = int(lm.x * w), int(lm.y * h)
+                if x > x_max:
+                    x_max = x
+                if x < x_min:
+                    x_min = x
+                if y > y_max:
+                    y_max = y
+                if y < y_min:
+                    y_min = y
+            y_min -= 20
+            y_max += 20
+            x_min -= 20
+            x_max += 20
+            cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+    
+    cv2.imshow("Frame", frame)
+
+cap.release()
+cv2.destroyAllWindows()
